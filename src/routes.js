a39a2dd744eb0cca6a4759c5de9f1b704dcfa56b
Rene Santiago <rsj510@gmail.com>
2023-03-11 22:55:26 -0500

    update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,20 @@
 /////////////////////////
 // Components Imports
 /////////////////////////
+import AllPosts from "./pages/AllPosts"
+import SinglePost from "./pages/SinglePost"
+import Form from "./pages/Form"
 
 /////////////////////////
 // Array of Routes
 /////////////////////////
 export default [
-    {path: "/", component: Home},
-    {path: "/", component: About}
+    // main page that shows all todos
+    {path: "/", component: AllPosts},
+    // page for seeing an individual todo
+    {path: "/post/:id", component: SinglePost, name: "post"},
+    // route for creating a new todo
+    {path: "/new", component: Form},
+    // route for updating a todo
+    {path: "/edit", component: Form},
 ]
