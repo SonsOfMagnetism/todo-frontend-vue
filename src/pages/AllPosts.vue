9c1940bd87f2f7815a8d2d5a2050aa4a66f63b4f
Rene Santiago <rsj510@gmail.com>
2023-03-12 00:31:02 -0500

    update allposts.vue

--- src/pages/AllPosts.vue
+++ src/pages/AllPosts.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
         <div class="post" v-for="(post, index) in $attrs.posts" v-bind:key="index">
-            <!-- <router-link :to="{name: 'post', params: {id, index}}"> -->
                 <h1>{{post.title}}</h1>
-            <!-- </router-link> -->
+                <input type="checkbox" v-model="post.completed">
         </div>
     </div>
 </template>
@@ -14,4 +13,11 @@
     }
 </script>
 
-<style></style>
+<style>
+    .post {
+        text-align: center;
+        border: 3px solid green;
+        margin: 10px auto;
+        width: 80%;
+    }
+</style>
