f9bda23e789e1646ec2ca97c72c46694b61e10c6
Rene Santiago <rsj510@gmail.com>
2023-03-11 23:22:09 -0500

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>
-    <h1>Our Todos</h1>
-    <router-view/>
+  <div class="app">
+    <h1>Shopping List</h1>
+    <router-view :posts="posts" :url="url" :getPosts="getPosts"/>
   </div>
 </template>
 
 <script>
+import {ref, onMounted} from "vue" // import compostion API Hooks
+  // ref hook allows the ability to create reactive variables
+  // onMounted alows the code to execute when component mounts
+
   export default {
-    name: "App"
+    name: "App",
+    // Setup property allows us to use new composition api to define properties/methods
+    // Returns an object with any properties/methods the component should have
+    setup(props) {
+      const url = "https://todosapp.herokuapp.com/todo"
+      const posts = ref([])
+      const getPosts = async () => {
+        const response = await fetch(url)
+        const data = await response.json()
+        posts.value = await data
+      }
+      onMounted(() => getPosts())
+      return {
+        posts,
+        getPosts,
+        url,
+        ...props,
+      }
+    }
   }
 </script>
 
