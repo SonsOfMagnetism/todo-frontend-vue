10d34ee95ba332782067de41b591f393bc854ac0
Rene Santiago <rsj510@gmail.com>
2023-03-14 20:29:31 -0400

    add delete function

--- src/pages/SinglePost.vue
+++ src/pages/SinglePost.vue
@@ -4,28 +4,39 @@
       <h2>{{post.completed}}</h2>
       <router-link to="/"><button>Back to Main</button></router-link>
       <router-link :to="{name: 'edit', params: {id: post.id}}"><button>Edit Todo</button></router-link>
+      <button v-on:click="deletePost">Delete Todo</button>
   </div>
 </template>
 
 <script>
 // get useRoute hook to access to params
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 // getting toRefs hook to maintain props reactivity
 import { toRefs } from 'vue';
 
     export default {
         name: "SinglePost",
-        props: ['posts'],
+        props: ["posts", "url", "getPosts"],
         setup(props){
             // get route object to access params
             const route = useRoute()
+            // get router
+            const router = useRouter()
             // retrieve posts from props
-            const {posts} = toRefs(props)
+            const {posts, url, getPosts} = toRefs(props)
             // grab target post from posts
             const post = posts.value[route.params.id]
+            const deletePost = async () => {
+                await fetch(url.value + post.id + "/", {
+                    method: "delete"
+                })
+                await getPosts.value()
+                router.push("/")
+            }
             // return properties
             return {
-                post
+                post,
+                deletePost
             }
         }
     }
@@ -33,6 +44,6 @@ import { toRefs } from 'vue';
 
 <style>
     button {
-        margin: 10px
+        margin: 10px;
     }
 </style>
